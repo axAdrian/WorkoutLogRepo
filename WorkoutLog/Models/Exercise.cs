00fd3e09fd73fe15f44763c6a84808a68742146c
Adrian <adioxoida@gmail.com>
2015-06-01 23:31:50 +0200

    Model - exercise

---
+++ WorkoutLog/Models/Exercise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkoutLog.Models
+{
+    public class Exercise
+    {
+        public int exerciseID { get; set; }
+        public string exerciseName { get; set; }
+        public string exerciseDesc { get; set; }
+    }
+}
