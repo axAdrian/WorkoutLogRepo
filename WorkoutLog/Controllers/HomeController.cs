cafa30997a0a8f86c7c9e4f3021342629f112f2d
Adrian <adioxoida@gmail.com>
2015-06-02 01:37:35 +0200

    Dzialajaca baza dla Training

--- WorkoutLog/Controllers/HomeController.cs
+++ WorkoutLog/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WorkoutLog.DAL;
+using WorkoutLog.Models;
 
 namespace WorkoutLog.Controllers
 {
     public class HomeController : Controller
     {
+        private LogContext db = new LogContext(); //do polaczenia z baza 
+
         public ActionResult Index()
         {
+            //przykladowa tabela w bazie
+            Training newTraining = new Training { Date = DateTime.Today, trainingName = "najebator"};
+            db.Trainings.Add(newTraining);
+            db.SaveChanges();
+
             return View();
         }
 
---
+++ WorkoutLog/DAL/LogContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WorkoutLog.Models;
+
+namespace WorkoutLog.DAL
+{
+    public class LogContext : DbContext
+    {
+        public LogContext() : base("LogContext")
+        {
+            
+        }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Training> Trainings { get; set; } 
+    }
+}
