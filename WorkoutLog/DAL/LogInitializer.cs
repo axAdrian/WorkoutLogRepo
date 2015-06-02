fc95a572b6b56dfc0aa43c875a92e3b7ff672443
Adrian <adioxoida@gmail.com>
2015-06-02 14:46:32 +0200

    Model - logInitializer

---
+++ WorkoutLog/DAL/LogInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using WorkoutLog.Models;
+
+namespace WorkoutLog.DAL
+{
+    public class LogInitializer : DropCreateDatabaseAlways<LogContext>
+    {
+        protected override void Seed(LogContext context)
+        {
+            SeedLogContext(context);
+            base.Seed(context);
+        }
+
+        private void SeedLogContext(LogContext context)
+        {
+            var tr = new List<Training>
+            {
+                new Training() {Date = DateTime.Today, trainingName = "plecy"},
+                new Training() {Date = DateTime.Today, trainingName = "nogi"}
+            };
+
+            tr.ForEach(g => context.Trainings.AddOrUpdate(g));
+            context.SaveChanges();
+        }
+    }
+}
